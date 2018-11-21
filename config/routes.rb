4df0f52271964a557f4cf12b994d3612aafa8a22
Maxime Zarka <maxime.zarka@gmail.com>
2018-11-21 10:02:52 +0100

    advanced

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :restaurants do
+    collection do
+      get 'best', to: 'restaurants#best'
+    end
+
+    member do
+      get 'chef', to: 'restaurants#chef'
+    end
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
